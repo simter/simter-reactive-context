64996da01821ce8999724ace1af1069d14f152a4
RJ.HWang <rongjihuang@gmail.com>
2018-08-14 13:44:56 +0800

    Add ReactorOperatorTest

---
+++ src/test/kotlin/tech/simter/reactive/reactor/ReactorOperatorTest.kt
@@ -0,0 +1,45 @@
+package tech.simter.reactive.reactor
+
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Mono
+import java.lang.RuntimeException
+
+/**
+ * @author RJ
+ */
+@Disabled
+class ReactorOperatorTest {
+  @Test
+  fun empty_then() {
+    // then 方法只是传输 completion 信号給其参数的结果
+    // 也就是说 then 会等待其参数的 mono 实例执行完毕
+    // 即 then 方法是肯定会执行的
+    Mono.empty<Int>()
+      .doOnNext { println("0-$it") } // never invoke
+      .then(Mono.just(true).doOnNext { println("1-$it") })
+      .doOnNext { println("2-$it") }
+      .subscribe()
+  }
+
+  @Test
+  fun error_then() {
+    try {
+      // error 的 then 函数不会执行
+      // 因此将 data 方法包装在 flatMap 内可以避免 error 时执行
+      Mono.error<Int>(RuntimeException())
+        .doOnNext { println("0-$it") } // never invoke
+        .then(Mono.just(true).flatMap { data(1) }.doOnNext { println("1-$it") })
+        .doOnNext { println("2-$it") }
+        .thenEmpty {  }
+        .subscribe()
+    } catch (e: Exception) {
+      println(e.javaClass)
+    }
+  }
+
+  private fun data(id: Int): Mono<Int> {
+    println("in data: id=$id")
+    return Mono.just(id)
+  }
+}
